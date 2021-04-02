92589b377f4c6c55f03ef0b2aff94abd2f6eed48
useralexey2000 <useralexey2000@gmail.com>
2021-04-02 14:01:22 +0300

    Adding redis pubsub for redistribution

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/useralexey2000/go-websocket-simple-chat
 go 1.15
 
 require (
+	github.com/go-redis/redis/v8 v8.8.0
 	github.com/gorilla/sessions v1.2.1
 	github.com/gorilla/websocket v1.4.2
+	google.golang.org/protobuf v1.25.0 // indirect
+	honnef.co/go/tools v0.1.3 // indirect
 )
--- main.go
+++ main.go
@@ -4,7 +4,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"encoding/json"
 
+	"context"
+
+	"github.com/go-redis/redis/v8"
 	"github.com/gorilla/sessions"
 	"github.com/gorilla/websocket"
 )
@@ -19,9 +23,42 @@ var upgrader = websocket.Upgrader{
 
 var secretkey = "myauthkey"
 var store = sessions.NewCookieStore([]byte(secretkey), nil)
+var redisAddr = "192.168.0.10:6379"
 
 func main() {
-	chat := NewChat()
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     redisAddr,
+		Password: "",
+		DB:       0,
+	})
+	ctx := context.Background()
+	pong, err := redisClient.Ping(ctx).Result()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(pong)
+	//
+	//	pubsub := redisClient.Subscribe(ctx, "chat")
+	//
+	//	_, err = pubsub.Receive(ctx)
+	//	if err != nil {
+	//		panic(err)
+	//	}
+	//	ch := pubsub.Channel()
+	//
+	//	err = redisClient.Publish(ctx, "chat", "hello world").Err()
+	//	if err != nil {
+	//		panic(err)
+	//	}
+	//	time.AfterFunc(time.Second, func() {
+	//		_ = pubsub.Close()
+	//	})
+	//
+	//	for msg := range ch {
+	//		fmt.Println(msg.Channel, msg.Payload)
+	//	}
+	//
+	chat := NewChat(redisClient)
 	go chat.Run()
 	http.HandleFunc("/", indexHandeler)
 	http.HandleFunc("/chat", chatHandler)
@@ -33,14 +70,16 @@ func main() {
 type Message struct {
 	Username string `json:"username"`
 	Text     string `json:"text"`
+	RoomID   string
 }
 
 // User ...
 type User struct {
-	Name string
-	Ch   chan Message
-	Cht  *Chat
-	Conn *websocket.Conn
+	Name   string
+	Ch     chan Message
+	RoomID string
+	Cht    *Chat
+	Conn   *websocket.Conn
 }
 
 func (user *User) write() {
@@ -63,41 +102,117 @@ func (user *User) read() {
 			return
 		}
 		msg.Username = user.Name
+		msg.RoomID = user.RoomID
 		user.Cht.broadcast <- msg
 	}
 }
 
-type Chat struct {
-	users     map[string]*User
-	reg       chan *User
-	unreg     chan *User
-	broadcast chan Message
+type Room struct {
+	id     string
+	users  map[string]*User
+	pubsub *redis.PubSub
 }
 
-func NewChat() *Chat {
+type Chat struct {
+	rooms       map[string]*Room
+	reg         chan *User
+	unreg       chan *User
+	broadcast   chan Message
+	redisClient *redis.Client
+}
+
+func NewChat(rc *redis.Client) *Chat {
 	return &Chat{
-		users:     make(map[string]*User),
-		reg:       make(chan *User),
-		unreg:     make(chan *User),
-		broadcast: make(chan Message),
+		rooms:       make(map[string]*Room),
+		reg:         make(chan *User, 0),
+		unreg:       make(chan *User, 0),
+		broadcast:   make(chan Message, 0),
+		redisClient: rc,
+	}
+}
+
+func (chat *Chat) AddRoom(id string) error {
+	if _, ok := chat.rooms[id]; ok {
+		//TODO the room already exist
+		return nil
+	}
+	ctx := context.Background()
+	pubsub := chat.redisClient.Subscribe(ctx, id)
+	_, err := pubsub.Receive(ctx)
+	if err != nil {
+		pubsub.Close()
+		return err
+	}
+	r := &Room{
+		id:     id,
+		users:  make(map[string]*User),
+		pubsub: pubsub,
+	}
+	chat.rooms[id] = r
+	go func() {
+		ch := pubsub.Channel()
+		for msg := range ch {
+			var m Message
+			err := json.Unmarshal([]byte(msg.Payload), &m)
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+			for _, u := range r.users {
+				u.Ch <- m
+			}
+		}
+	}()
+	return nil
+}
+
+func (chat *Chat) RemRoom(id string) {
+	if _, ok := chat.rooms[id]; ok {
+		chat.rooms[id].pubsub.Close()
+		delete(chat.rooms, id)
 	}
 }
 
 func (chat *Chat) Run() {
+	//Default room number
+	err := chat.AddRoom("default")
+	if err != nil {
+		panic(err)
+	}
 	for {
 		select {
 		case user := <-chat.reg:
-			chat.users[user.Name] = user
+			// TODO add room if user entered with other room num than default
+			r := chat.rooms[user.RoomID]
+			r.users[user.Name] = user
+			// broadcast to pubsub user reg
 		case user := <-chat.unreg:
-			if _, ok := chat.users[user.Name]; ok {
-				delete(chat.users, user.Name)
+			r := user.RoomID
+			if _, ok := chat.rooms[r].users[user.Name]; ok {
+				delete(chat.rooms[r].users, user.Name)
 				user.Conn.Close()
+				if len(chat.rooms[r].users) == 0 {
+					chat.RemRoom(r)
+				}
+				// broadcast to pubsub user unreg
 			}
+			//		case msg := <-chat.broadcast:
+			//			for _, u := range chat.users {
+			//				fmt.Println("user sent message: ", msg.Username, msg.Text)
+			//				go func() { u.Ch <- msg }()
+			//			}
 		case msg := <-chat.broadcast:
-			for _, u := range chat.users {
-                fmt.Println("user sent message: ", msg.Username, msg.Text)
-				go func() { u.Ch <- msg }()
+			ctx := context.Background()
+			// TODO encode msg to redis format
+			err := chat.redisClient.Publish(ctx, msg.RoomID, msg).Err()
+			if err != nil {
+				panic(err)
 			}
+			//		case msg:= <-chat.pubsubChan
+			//			fmt.Println(msg.Channel, msg.Payload)
+			//			for _, u := range chat.users {
+			//				go func() { u.Ch <- msg }()
+			//			}
 		}
 	}
 }
@@ -145,7 +260,7 @@ func chatHandler(w http.ResponseWriter, r *http.Request) {
 func wsHandler(chat *Chat) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("in wsHandler")
-        sess, err := store.Get(r, "login")
+		sess, err := store.Get(r, "login")
 		if err != nil {
 			log.Println("cant get sess: ", err)
 			return
@@ -157,13 +272,15 @@ func wsHandler(chat *Chat) http.HandlerFunc {
 			return
 		}
 		user := &User{
-			Name: uname,
-			Ch:   make(chan Message),
-			Cht:  chat,
-			Conn: conn,
+			RoomID: "default",
+			Name:   uname,
+			Ch:     make(chan Message, 0),
+			Cht:    chat,
+			Conn:   conn,
 		}
 		// TODO check map if user with such name exists
-		chat.users[user.Name] = user
+		// chat.rooms[user.RoomID].users[user.Name] = user
+		chat.reg <- user
 		fmt.Println("user added: ", user)
 		go user.read()
 		go user.write()
